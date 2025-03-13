4345b1239531fafe5a90cdd4ac99373a3ab1071b
Bartek Jaruga <245827@edu.p.lodz.pl>
2025-03-13 13:44:55 +0100

    fixed showing simulation in main application window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -3,16 +3,20 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class MyAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private final DrawPanelController drawPanelControler;
 
-	public MyAdapter() {
+	public MyAdapter(DrawPanelController drawPanelController) {
 		super();
+		this.drawPanelControler = drawPanelController;
 	}
 
 	@Override
@@ -28,7 +32,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setEndCoordinates(x, y);
 		setPosition(x, y);
 
-		drawLine(line);
+		drawPanelControler.drawLine(line);
 	}
 
 	@Override
