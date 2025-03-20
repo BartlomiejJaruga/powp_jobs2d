f90481d9e383db81896ac037e518bd4b972aa2c9
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-03-20 19:32:09 +0100

    Some unused imports deleted.

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,13 +2,8 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 import java.util.Objects;
 
-import edu.kis.powp.command.ComplexCommand;
-import edu.kis.powp.command.DriverCommand;
-import edu.kis.powp.command.OperateToCommand;
-import edu.kis.powp.command.SetPositionCommand;
 import edu.kis.powp.factories.FiguresFactory;
 import edu.kis.powp.jobs2d.Job2dTestsEnum;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
