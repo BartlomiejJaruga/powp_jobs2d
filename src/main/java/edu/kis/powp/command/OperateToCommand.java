ffce4961c293412d2195cb09bde4ac4c6b911c55
Bartek Jaruga <245827@edu.p.lodz.pl>
2025-03-20 13:43:26 +0100

    Command Driver implementation changed, now you can pass Driver to execute() method in order to execute SPC or OTC with various drivers without creating new object.

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -4,16 +4,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class OperateToCommand implements DriverCommand {
     private int x, y;
-    private Job2dDriver driver;
 
-    public OperateToCommand(int x, int y, Job2dDriver driver) {
+    public OperateToCommand(int x, int y) {
         this.x = x;
         this.y = y;
-        this.driver = driver;
     }
 
     @Override
-    public void execute() {
+    public void execute(Job2dDriver driver) {
         driver.operateTo(x, y);
     }
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -4,16 +4,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class SetPositionCommand implements DriverCommand {
     private int x, y;
-    private Job2dDriver driver;
 
-    public SetPositionCommand(int x, int y, Job2dDriver driver) {
+    public SetPositionCommand(int x, int y) {
         this.x = x;
         this.y = y;
-        this.driver = driver;
     }
 
     @Override
-    public void execute() {
+    public void execute(Job2dDriver driver) {
         driver.setPosition(x, y);
     }
 }
